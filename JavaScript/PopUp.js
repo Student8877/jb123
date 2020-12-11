a5f2b28ab1242c50dfd34f57677ecb15d343fa90
Student8877 <73313441+Student8877@users.noreply.github.com>
2020-12-10 22:36:31 -0700

    Add files via upload

---
+++ JavaScript/PopUp.js
@@ -0,0 +1,9 @@
+function openForm()
+{
+  document.getElementsByClassName("popUpForm")[0].style.display = "block";
+}
+
+function closeForm()
+{
+  document.getElementsByClassName("popUpForm")[0].style.display = "none";
+}
